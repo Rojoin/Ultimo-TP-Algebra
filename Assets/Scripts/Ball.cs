f8fe958af01691d011ab9a94c0f2d2daa6183c2c
Mateo <matufabbri@gmail.com>
2022-11-14 11:18:59 -0300

    Add ball map collision

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [Header("Ball")]
     [SerializeField] private float radius;
     [Tooltip("0 means no drag. 1 means instant stop")]
     [SerializeField] private float drag;
     [SerializeField] private Vector2 initVel;
+
+    [Header("Map")]
+    [SerializeField] private Transform topLeft;
+    [SerializeField] private Transform lowRight;
 
     private Vector3 accel;
     private Vector3 vel;
@@ -23,7 +28,7 @@ public class Ball : MonoBehaviour
     void Update()
     {
         UpdateSize();
-
+        MapCollide();
         Move();
         Drag();
     }
@@ -46,12 +51,39 @@ public class Ball : MonoBehaviour
 
     void UpdateSize()
     {
-        Vector2 newScale = new Vector2(radius, radius);
+        Vector2 newScale = new Vector2(radius * 2, radius * 2);
         transform.localScale = newScale;
     }
 
     void Move()
     {
         transform.position += vel * Time.deltaTime;
+    }
+
+    void MapCollide()
+    {
+        if (transform.position.x - radius <= topLeft.position.x)
+        {
+            vel.x *= -1;
+            transform.position = new Vector3(topLeft.position.x + radius, transform.position.y, 0);
+        }
+
+        if (transform.position.x + radius >= lowRight.position.x)
+        {
+            vel.x *= -1;
+            transform.position = new Vector3(lowRight.position.x - radius, transform.position.y, 0);
+        }
+
+        if (transform.position.y + radius >= topLeft.position.y)
+        {
+            vel.y *= -1;
+            transform.position = new Vector3(transform.position.x, topLeft.position.y - radius, 0);
+        }
+
+        if (transform.position.y - radius <= lowRight.position.y)
+        {
+            vel.y *= -1;
+            transform.position = new Vector3(transform.position.x, lowRight.position.y + radius, 0);
+        }
     }
 }
